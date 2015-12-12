a1ffcc59e758d93c72e01ef33062037f1269c708
juanbono <juanbono94@gmail.com>
2015-12-12 17:31:24 -0300

    Saco el Iteratee de logging porque ya no se usa mas.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,18 +1,11 @@
 package controllers
 
 import actors.TwitterStreamer
-import play.api.libs.iteratee._
-import play.api.libs.json.{JsValue, JsObject}
-import play.api._
+import play.api.libs.json.JsValue
 import play.api.Play.current
-import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc._
 
 class Application extends Controller {
-
-  val loggingIteratee = Iteratee.foreach[JsObject] { value =>
-    Logger.info(value.toString)
-  }
 
   def index = Action { implicit request =>
     Ok(views.html.index("Tweets"))
